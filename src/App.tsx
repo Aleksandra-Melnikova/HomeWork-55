11f49a5d83facf3587b2bc2e39a6a6702387696d
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-25 15:54:58 +0600

    Create component ButtonImage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import './App.css';
+import meat from './assets/meat.png';
+import cheese from './assets/cheese.png';
+import salad from './assets/salad.png';
+import bacon from './assets/bacon.png';
+import Ingredient from "./components/Ingredient/Ingredient.tsx";
+
+type IngredientType = {
+    title: string;
+    cost: number;
+    src: string;
+    key:string
+}
+
+interface IIngredients {
+    name: string ;
+    count: number;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const ingredientsItem: IngredientType[] = [
+        {title: 'Meat', cost: 80, src: meat, key:String(Math.random() * 1000)},
+        {title: 'Cheese', cost: 50, src: cheese, key:String(Math.random() * 1000)},
+        {title: 'Salad', cost: 10, src: salad, key:String(Math.random() * 1000)},
+        {title: 'Bacon', cost: 60, src: bacon, key:String(Math.random() * 1000)}
+    ];
+
+    const [ingredients, setIngredients] = useState<{}>({
+        ingredients: 0,
+        Cheese: 0,
+        Salad: 0,
+        Bacon: 0,}
+    );
+
+    // const AddIngredientToBurger = (e) =>{
+    //
+    // }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="content">
+            <div className='content-item'><h2>Ingredients</h2>
+                {ingredientsItem.map((ingredient) =>
+            <Ingredient onClickAdd={AddIngredientToBurger} key = {ingredient.key} name = {ingredient.title} count = {0} src={ingredient.src} />)}
+            </div>
+            <div>
+                <h2>Burger</h2>
+                <div className="Burger">
+                    <div className="BreadTop">
+                        <div className="Seeds1"></div>
+                        <div className="Seeds2"></div>
+                    </div>
+                    <div className="Salad"></div>
+                    <div className="Cheese"></div>
+                    <div className="Meat"></div>
+                    <div className="BreadBottom"></div>
+                </div>
+                <span className='price'> Price: 0 </span>
+            </div>
+        </div>
     </>
   )
 }
